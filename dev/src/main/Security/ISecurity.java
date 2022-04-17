3e9e7401f9f4b884ea95f276911af738ab972be0
vladiodes <vladiodes@gmail.com>
2022-04-17 12:16:30 +0300

    Implemented Security class - crypto hashing function, implemented using the SHA 512 algorithm. Added unit tests for the class - all passing

--- dev/src/main/Security/ISecurity.java
+++ dev/src/main/Security/ISecurity.java
@@ -1,4 +1,12 @@
 package main.Security;
 
+
 public interface ISecurity {
+    /***
+     *
+     * @param password - password to hash.
+     * @return hashed password.
+     */
+    String hashPassword(String password);
+
 }
