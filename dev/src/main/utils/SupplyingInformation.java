f0edacde144cc7db7de085d397af8c417c32cb92
sashajke <62395693+sashajke@users.noreply.github.com>
2022-04-25 18:41:55 +0300

    System Req AT finished (#44)
    * System Req AT finished
    
    * Added system Req AT
    
    * added 10.2 test
    
    * Delete Workshop-on-Software-Engineering-Project.iml
    
    * Delete LOG.txt
    
    * Delete .name
    
    * Delete LOG.txt
    
    * Delete dev.iml

--- dev/src/main/utils/SupplyingInformation.java
+++ dev/src/main/utils/SupplyingInformation.java
@@ -1,5 +1,6 @@
 package main.utils;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class SupplyingInformation {
---
+++ dev/src/test/AcceptanceTests/AT_Req_1_1.java
@@ -0,0 +1,55 @@
+package test.AcceptanceTests;
+
+import main.DTO.UserDTO;
+import main.Service.IService;
+import main.Service.Service;
+import main.utils.PaymentInformation;
+import main.utils.Response;
+import main.utils.SupplyingInformation;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AT_Req_1_1 {
+
+    IService service;
+    Response<String> adminToken;
+    PaymentInformation pInfo;
+    SupplyingInformation sInfo;
+    Response<String> founder1token;
+
+    @Before
+    public void setUp() {
+        service = new Service();
+        adminToken = service.guestConnect();
+        pInfo = new PaymentInformation(true);
+        sInfo = new SupplyingInformation(true);
+        founder1token = service.guestConnect();
+        service.register("founder1", "12345678");
+        service.login(founder1token.getResult(), "founder1", "12345678");
+        service.openStore(founder1token.getResult(), "MyStore1");
+        service.addProductToStore(founder1token.getResult(), "Coca Cola", "Drinks", null, "tasty drink", "MyStore1", 100, 6);
+
+
+    }
+    /*
+        Functional Req 1.1 - check that there is a system admin
+     */
+    @Test
+    public void checkAdminExistence() {
+        Response<UserDTO> res = service.login(adminToken.getResult(),"admin","admin");
+        assertFalse(res.isError_occured() && res.getResult() == null);
+    }
+    @Test
+    public void checkPaymentAndSupplySystemsAreAccesible() {
+        Response<UserDTO> resLogin = service.login(adminToken.getResult(),"admin","admin");
+        Response<Boolean> resAddToCart = service.addProductToCart(adminToken.getResult(),"MyStore1","Coca Cola", 5);
+        Response<Boolean> resPurchase = service.purchaseCart(adminToken.getResult(),pInfo,sInfo);
+        assertTrue(resPurchase.isError_occured() == false);
+    }
+}
